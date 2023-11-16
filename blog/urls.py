aff1ea541706cdb33daa98902104c7fb970f4714
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-11-16 19:43:37 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('', IndexView.as_view(), name='index'),
 ]
