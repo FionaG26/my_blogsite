0e3fd4d644bcaac79df285813660d872c8d2835f
FionaG26 <fionamuthoni18@gmail.com>
2023-11-16 15:58:55 +0300

    directory created

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+# my_blogsite/blog/forms.py
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
