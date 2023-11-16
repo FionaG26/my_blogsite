496c5067951a71d8d89c6251c10d72fa7ebaeda1
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-11-16 15:07:01 +0300

    views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
-# Create your views here.
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+]
