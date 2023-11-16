2cbb3f7cb86ab139ad6a660c86061214da2f652f
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-11-16 15:27:24 +0300

    Update settings.py

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'FionaG26.github.io']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
