75417529c649cf2ffd838c1f41f3f38f84cb6eef
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-11-17 19:05:43 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, IndexView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('', IndexView.as_view(), name='index'),  # Move the index path to the top
+    path('post/', PostListView.as_view(), name='post_list'),  # Add 'post/' to the path
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('', IndexView.as_view(), name='index'),
 ]
